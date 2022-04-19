41c10e9a486b1559b7e92b91eedf8e39cf39b3e2
mamonde456 <81732659+mamonde456@users.noreply.github.com>
2022-04-19 14:27:16 +0900

    2022-04-19

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="red" bgColor="tomato" />
+      <Circle text="im here" bgColor="black" />
+    </div>
+  );
+}
+
+export default App;
